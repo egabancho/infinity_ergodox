6865447f0ede9f96129248b7fb1516b04a49c8f1
Esteban J. G. Gabancho <esteban.gabancho@gmail.com>
2016-11-20 18:52:34 +0100

    default layer + actions

--- keymap_plain.c
+++ keymap_plain.c
@@ -21,91 +21,37 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP(  // layer 0 : default
         // left hand
-        EQL, 1,   2,   3,   4,   5,   ESC,
-        BSLS,Q,   W,   E,   R,   T,   FN1,
-        TAB, A,   S,   D,   F,   G,
-        LSFT,Z,   X,   C,   V,   B,   FN0,
-        LGUI,GRV, BSLS,LEFT,RGHT,
-                                      LCTL,LALT,
+        EQL,  1,   2,   3,   4,   5,   ESC,
+        TAB,  Q,   W,   E,   R,   T,   LBRC,
+        FN3, A,   S,   D,   F,   G,
+        FN5, Z,   X,   C,   V,   B,    FN7,
+        FN1, FN2, LCTL,LALT,LGUI,
+                                      FN9, FN10,
                                            HOME,
-                                 BSPC,DEL, END,
+                                 SPC,BSPC, END,
         // right hand
-             FN2, 6,   7,   8,   9,   0,   MINS,
-             LBRC,Y,   U,   I,   O,   P,   RBRC,
-                  H,   J,   K,   L,   SCLN,QUOT,
-             FN0, N,   M,   COMM,DOT, SLSH,RSFT,
-                       LEFT,DOWN,UP,  RGHT,RGUI,
-        RALT,RCTL,
+        GRV,  6,   7,   8,    9,   0,    MINS,
+        RBRC, Y,   U,   I,    O,   P,    RBRC,
+              H,   J,   K,    L,   SCLN, FN4,
+        FN8,  N,   M,   COMM, DOT, SLSH, FN6,
+                   LEFT,DOWN, UP,  RGHT, FN1,
+        FN11, FN12,
         PGUP,
-        PGDN,ENT, SPC
-    ),
-
-    KEYMAP(  // layer 1 : function and symbol keys
-        // left hand
-        TRNS,F1,  F2,  F3,  F4,  F5,  F11,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,FN3,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,
-                                      TRNS,TRNS,
-                                           TRNS,
-                                 TRNS,TRNS,TRNS,
-        // right hand
-             F12, F6,  F7,  F8,  F9,  F10, TRNS,
-             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-                  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-                       TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,
-        TRNS,
-        TRNS,TRNS,TRNS
-    ),
-
-    KEYMAP(  // layer 2 : keyboard functions
-        // left hand
-        BTLD,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, FN3,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,
-                                      TRNS,TRNS,
-                                           TRNS,
-                                 TRNS,TRNS,TRNS,
-        // right hand
-             TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-                  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-                       TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,
-        TRNS,
-        TRNS,TRNS,TRNS
-    ),
-
-    KEYMAP(  // layer 3: numpad
-        // left hand
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,
-                                      TRNS,TRNS,
-                                           TRNS,
-                                 TRNS,TRNS,TRNS,
-        // right hand
-             TRNS,NLCK,PSLS,PAST,PAST,PMNS,BSPC,
-             TRNS,NO,  P7,  P8,  P9,  PMNS,BSPC,
-                  NO,  P4,  P5,  P6,  PPLS,PENT,
-             TRNS,NO,  P1,  P2,  P3,  PPLS,PENT,
-                       P0,  PDOT,SLSH,PENT,PENT,
-        TRNS,TRNS,
-        TRNS,
-        TRNS,TRNS,TRNS
+        PGDN, TAB, ENT
     ),
 };
 const uint16_t fn_actions[] = {
-    ACTION_LAYER_MOMENTARY(1),                      // FN0 - switch to Layer1
-    ACTION_LAYER_SET(2, ON_PRESS),                  // FN1 - set Layer2
-    ACTION_LAYER_TOGGLE(3),                         // FN2 - toggle Layer3 aka Numpad layer
-    ACTION_LAYER_SET(0, ON_PRESS),                  // FN3 - set Layer0
+  ACTION_LAYER_SET(0, ON_PRESS),
+  ACTION_LAYER_MOMENTARY(1), // layer 1 : fn                       FN1
+  ACTION_LAYER_MOMENTARY(2), // layer 2: keypad                    FN2
+  ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC), // control / esc          FN3
+  ACTION_MODS_TAP_KEY(MOD_RCTL, KC_QUOT), // control / "           FN4
+  ACTION_MODS_TAP_TOGGLE(MOD_LSFT), // shift                       FN5
+  ACTION_MODS_TAP_TOGGLE(MOD_RSFT), // shift                       FN6
+  ACTION_MODS(MOD_LCTL | MOD_LALT | MOD_LSFT | MOD_LGUI), // hyper FN7
+  ACTION_MODS(MOD_LCTL | MOD_LALT | MOD_LSFT), // meh              FN8
+  ACTION_MODS_KEY(MOD_LCTL, KC_SPC), // tmux bind                  FN9
+  ACTION_MODS_KEY(MOD_LGUI, KC_SPC), // spotligh                   FN10
+  ACTION_MODS_KEY(MOD_LGUI, KC_C), // tmux bind                    FN11
+  ACTION_MODS_KEY(MOD_LGUI, KC_V), // tmux bind                    FN12
 };
